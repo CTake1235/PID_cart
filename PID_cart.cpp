e56f0121a6932ae48c551522583493d5fa7e62cd
ctake <ctake1235@gmail.com>
2023-04-03 22:47:09 +0900

    first commit

---
+++ PID_cart.cpp
@@ -0,0 +1,45 @@
+/*
+超音波センサーとPIDを使って台車の位置をｴｴ感じにするプログラム
+参考：https://www.youtube.com/watch?v=oy58S4beC9c
+*/
+#include "mbed.h"
+#include "hcsr04.h"
+#include "PID.h"
+#include <type_traits>
+
+#define add 0x20
+
+HCSR04  fuga(NC, NC);           //超音波センサー
+Thread  hoge;                   //I2C送信用
+PID     controller(2,0,0,0.1);  //
+I2C     i2c(D14,D15);
+
+unsigned int dist;
+char power;
+
+void send(char, char);
+
+int main(void){
+    dist = 0;
+    // PIDの設定たち
+    controller.setInputLimits(0, 30);       //ブツの長さによる
+    controller.setOutputLimits(0x80,0xff);  //モーターにかけるduty比
+    controller.setSetPoint(5);              //目的とする距離[cm]
+    // PIDの設定、以上
+    while(true){
+        fuga.start();
+        wait_us(50000);
+        dist = fuga.get_dist_cm();
+        controller.setProcessValue(dist);
+        power = controller.compute();
+        send(add,power);
+    }
+}
+
+void send(char address,char duty){
+    i2c.start();
+    i2c.write(address);
+    i2c.write(duty);
+    i2c.stop();
+    ThisThread::sleep_for(10ms);
+}
